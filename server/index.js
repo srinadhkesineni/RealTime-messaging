bead9845170e69952154f0a1e12bdacfd563a0fb
Srinadh kesineni <srinadhkesineni7@gmail.com>
2025-03-29 23:43:22 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+// const { Socket } = require("dgram");
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID ${socket.id} joined room:${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconencted", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("server listening on port 3001");
+});
