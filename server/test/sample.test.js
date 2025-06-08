4f2ea301510054bfaf8f124d7c0db25a891f5afd
Srinadh kesineni <srinadhkesineni7@gmail.com>
2025-06-08 16:08:15 +0530

    sample tests added

--- server/test/sample.test.js
+++ server/test/sample.test.js
@@ -1,7 +1,19 @@
 const assert = require('assert');
 
-describe('Sample Test', () => {
-  it('should return true', () => {
-    assert.strictEqual(true, true);
+describe('Math operations', () => {
+  it('should add numbers correctly', () => {
+    assert.strictEqual(2 + 3, 5);
+  });
+
+  it('should subtract numbers correctly', () => {
+    assert.strictEqual(10 - 4, 6);
+  });
+
+  it('should multiply numbers correctly', () => {
+    assert.strictEqual(3 * 4, 12);
+  });
+
+  it('should divide numbers correctly', () => {
+    assert.strictEqual(8 / 2, 4);
   });
 });
