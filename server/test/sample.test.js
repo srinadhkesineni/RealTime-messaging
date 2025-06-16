a64527a9fc78f1275417c82fc08dcb53cc3603c2
Srinadh kesineni <srinadhkesineni7@gmail.com>
2025-06-16 18:21:00 +0530

    changes made

--- server/test/sample.test.js
+++ server/test/sample.test.js
@@ -1,19 +1,19 @@
-const assert = require('assert');
+const assert = require("assert");
 
-describe('Math operations', () => {
-  it('should add numbers correctly', () => {
-    assert.strictEqual(2 + 3, 5);
+describe("Math operations", () => {
+  it("should add numbers correctly", () => {
+    assert.strictEqual(2 + 3, 69);
   });
 
-  it('should subtract numbers correctly', () => {
+  it("should subtract numbers correctly", () => {
     assert.strictEqual(10 - 4, 6);
   });
 
-  it('should multiply numbers correctly', () => {
+  it("should multiply numbers correctly", () => {
     assert.strictEqual(3 * 4, 12);
   });
 
-  it('should divide numbers correctly', () => {
+  it("should divide numbers correctly", () => {
     assert.strictEqual(8 / 2, 4);
   });
 });
